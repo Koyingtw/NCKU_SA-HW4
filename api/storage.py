d239f4ebc7b3f2e919a23ddcbd96092c423bf863
Koying <koyingchen0523@gmail.com>
2023-06-07 02:23:54 +0800

    try to fix 409 response

--- api/storage.py
+++ api/storage.py
@@ -144,6 +144,21 @@ class Storage:
         parity_file = (
             f"/var/raid/block-{n - 1}/{file.filename}"  # 奇偶校驗檔案的檔名，例如 parity.bin
         )
+
+        if os.path.exists(parity_file):
+            with open(parity_file, "rb") as f:
+                old_parity = f.read()
+                if (
+                    hashlib.md5(parity_block).hexdigest()
+                    == hashlib.md5(old_parity).hexdigest()
+                ):
+                    detail = {"detail": "File already exists"}
+                    response = Response(
+                        content=json.dumps(detail), status_code=status.HTTP_409_CONFLICT
+                    )
+                    response.headers["Content-Type"] = "application/json"
+                    return response
+
         with open(parity_file, "wb") as f:
             f.write(parity_block)
 
