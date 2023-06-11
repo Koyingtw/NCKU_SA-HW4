ab61c34b9bf911a17cd112cb42f888c9b27804ed
Koying <koyingchen0523@gmail.com>
2023-06-11 15:22:24 +0800

    create file 的 or 改回 and

--- api/storage.py
+++ api/storage.py
@@ -42,7 +42,7 @@ class Storage:
         # 1. all data blocks must exist
         num_disks = settings.NUM_DISKS
 
-        for i in range(num_disks - 1):
+        for i in range(num_disks):
             if not os.path.exists(f"/var/raid/block-{i}/{filename}"):
                 print(f"/var/raid/block-{i}/{filename} Not exist")
                 await self.delete_file(filename)
